b02a7350ce9a8a858e4d61410381192bc60bf3a8
alf, server <sites@hubmed.org>
2009-01-03 16:45:25 -0500

    fixed sidebar javascript

--- js/script.js
+++ js/script.js
@@ -1,7 +1,28 @@
-var searchBox;
+function searchLoaded(){    
+  var reset = $("<a href='sidebar.php'>Reset</a>");
+  $("#sidebar").prepend(reset);
+  reset.click(function(e){
+    e.preventDefault();
+    $("#sidebar").load("sidebar.php");
+    return false;
+  });
+}
 
-$().ready(function(){
-  searchBox = $("#search").clone();
+function init(){
+  var searchBox = $("#search");
+  
+  var input = $("input:text", searchBox);
+  var submit = $("input:submit", searchBox);
+  
+  console.log(input.val());
+  if (!input.val())
+    input.val(submit.val()).click(function(e){
+    	var input = $(e.target);
+    	var submit = $("input:submit", input.parent());
+    	if (input.val() == submit.val())
+    		input.val("");
+    });
+  //submit.hide();
 
   $("a.player").click(function(e){
   //$("a.player").live("click", function(e){
@@ -13,20 +34,9 @@ $().ready(function(){
   $("#search").submit(function(e){
   //$("#search").live("submit", function(e){
     e.preventDefault();
+    location.hash = "#search";
     $("#sidebar").load($("#search").attr("action"), { q: $("#sidebar .q").val(), n: $("#sidebar .limit").val() }, searchLoaded);
     //return false;
-  });
-});
-
-function searchLoaded(){
-  $("#sidebar").prepend(searchBox);
-    
-  var reset = $("<a href='sidebar.php'>Reset</a>");
-  $("#sidebar").prepend(reset);
-  reset.click(function(e){
-    e.preventDefault();
-    $("#sidebar").load("sidebar.php");
-    return false;
   });
 }
 
--- sidebar.php
+++ sidebar.php
@@ -1,15 +1,22 @@
 <?php require_once 'includes/functions.inc.php'; ?>
 
-<form id="search" action="search.php" method="get">
-    <input class="q" type="text" name="q" size="30"/>
+<form id="search" action="sidebar.php" method="get">
+    <input class="q" type="text" name="q" size="30" value="<?php print filter_var($_REQUEST['q'], FILTER_SANITIZE_SPECIAL_CHARS);?>"/>
     <input class="limit" type="hidden" name="n" value="10"/>
     <input type="submit" value="search"/>
 </form>
 
 <?php
-$result = db_query("SELECT * FROM episodes WHERE date > %d ORDER BY date DESC LIMIT 10", time() - 60*60*24*7); // 1 week
+if ($_REQUEST['q'])
+  $result = db_query("SELECT * FROM episodes WHERE MATCH (title, subtitle, synopsis) AGAINST ('%s') LIMIT %d", $_REQUEST['q'], array_key_exists('n', $_REQUEST) ? $_REQUEST['n'] : 50);
+else
+  $result = db_query("SELECT * FROM episodes WHERE date > %d ORDER BY date DESC LIMIT 10", time() - 60*60*24*7); // 1 week
 
 while ($item = mysql_fetch_object($result))
   format_episode($item);
 
 ?>
+
+<script>
+$().ready(init);
+</script>
