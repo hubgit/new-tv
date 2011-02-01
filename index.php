0a6a754d61dd9aea53b101a584fb0a007f6b10e6
alf <sites@hubmed.org>
2011-02-01 00:32:32 +0000

    misc

--- index.php
+++ index.php
@@ -1,31 +1,83 @@
 <!DOCTYPE html>
-<?php 
-require dirname(__FILE__) . '/main.php'; 
+<?
+require dirname(__FILE__) . '/main.php';
 
 $iplayer = 'http://www.bbc.co.uk/iplayer/episode/';
   $agents = array('iphone', 'ipod');
   foreach ($agents as $agent)
     if (preg_match("/\b$agent\b/i", $_SERVER['HTTP_USER_AGENT']))
       $iplayer = 'http://www.bbc.co.uk/mobile/iplayer/episode/';
-  
+
   $page = $_REQUEST['page'] ? (int) $_REQUEST['page'] : 1;
-  
+
   $n = 50;
   $start = ($page - 1) * $n;
-  
+
   if ($q = $_REQUEST['q'])
     $result = $db->query("SELECT * FROM episodes WHERE MATCH (title, subtitle, synopsis) AGAINST ('%s') ORDER BY date DESC LIMIT %d, %d", $q, $start, $n);
   else
     $result = $db->query("SELECT * FROM episodes ORDER BY date DESC LIMIT %d, %d", $start, $n);
-    
+
   $old_date = 0;
   $ignores = array('news', 'weather', 'racing');
-  
+
 ?>
 <html>
 <head>
   <title>New TV</title>
-  <link rel=stylesheet href=style.css>
+  <link rel=stylesheet href=style.css?1>
+  <link rel=apple-touch-icon href=icon.png>
+  <meta name=viewport content=width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=0;>
+  <meta name=apple-mobile-web-app-capable content=yes>
+  <meta name=apple-mobile-web-app-status-bar-style content=black-translucent>
+</head>
+
+<body>
+  <nav>
+<? if ($page > 1): ?>
+    <a id=back href="./?page=<? h($page - 1); ?>">Back</a>
+<? endif; ?>
+    <a href=./><h1>New TV</h1></a>
+  </nav>
+
+  <form id=search>
+    <input name=q size=30 value="<? h($q); ?>"> <input type=submit value=search>
+  </form>
+
+  <ol id=programmes>
+<? while ($item = mysql_fetch_object($result)): ?>
+<? foreach ($ignores as $ignore) if (preg_match("/\b$ignore\b/i", $item->title)) continue(2); ?>
+<? $date = date('Y-m-d', $item->date); ?>
+<? $link = $iplayer . $item->episode; ?>
+
+<? if ($old_date != $date): $old_date = $date; ?>
+    <li>
+      <h2 class=date><? h($date); ?></h2>
+    </li>
+<? endif; ?>
+
+    <li class="episode<? if ($item->position == 1) print ' new-series'; else if ($item->position == 0) print ' single'; ?>">
+      <a class=thumbnail href="<? h($link); ?>">
+        <img width=150 height=84 class=episode-image src="http://www.bbc.co.uk/iplayer/images/episode/<? h($item->episode); ?>_150_84.jpg"/> <!-- also 640x360 -->
+      </a>
+
+      <div class=meta>
+        <div class=title>
+          <a href="<? h($link); ?>"><? h($item->title); ?></a>
+        </div>
+        <div class=synopsis><? h($item->synopsis); ?></div>
+<? if ($item->series && $item->position): ?>
+        <div class=series>Episode <span class=position><? h($item->position); ?><span><? if ($item->series_total): ?>/<span class=series-total><? h($item->series_total); ?></span><? endif; ?><? if ($item->series_position): ?>, Series <span class=series-position><? h($item->series_position); ?></span><? endif; ?></div>
+<? endif; ?>
+      </div>
+    </li>
+<? endwhile; ?>
+  </ol>
+
+  <nav>
+    <a id=more href="./?page=<? h($page + 1); ?>">More</a>
+  </nav>
+
   <script src=/jquery.js></script>
   <script>
     $().ready(function(){
@@ -34,58 +86,6 @@ $iplayer = 'http://www.bbc.co.uk/iplayer/episode/';
       });
     });
   </script>
-  
-  <link rel=apple-touch-icon href=icon.png>
-  <meta name=viewport content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0;">
-  <meta name=apple-mobile-web-app-capable content=yes>
-  <meta name=apple-mobile-web-app-status-bar-style content=black-translucent>
-</head>
-
-<body>
-    <nav>
-      <?php if ($page > 1): ?><a id=back href="./?page=<?php print $page - 1; ?>">Back</a><?php endif; ?>
-      <a href=./><h1>New TV</h1></a>
-    </nav>
-
-    <form id=search>
-      <input name=q size=30 value="<?php $api->output($q, 'attr'); ?>"> <input type=submit value=search>
-    </form>    
-
-    <table id=programmes>    
-<?php while ($item = mysql_fetch_object($result)): ?>
-<?php foreach ($ignores as $ignore) if (preg_match("/\b$ignore\b/i", $item->title)) continue(2); ?>
-<?php $date = date('Y-m-d', $item->date); ?>
-<?php $link = $iplayer . $item->episode; ?>
-     
-<?php if ($old_date != $date): $old_date = $date; ?><tr><td class="date" colspan="2"><h3><?php print $date; ?></h3></td></tr><?php endif; ?>
-      
-      <tr class="episode <?php if ($item->position == 1) print 'new-series'; else if ($item->position == 0) print 'single'; ?>">
-          <td class=thumbnail>
-            <a href="<?php $api->output($link, 'attr'); ?>">
-              <img width=150 height=84 class=episode-image src="http://www.bbc.co.uk/iplayer/images/episode/<?php $api->output($item->episode, 'attr'); ?>_150_84.jpg"/> <!-- also 640x360 -->
-            </a>
-          </div> 
-          
-          <td class=meta>
-              <div class=title>
-                <a href="<?php $api->output($link, 'attr'); ?>"><?php $api->output($item->title); ?></a>
-              </div>
-              
-              <div class=synopsis><?php $api->output($item->synopsis); ?><div>
-
-<?php if ($item->series && $item->position): ?>
-              <div class=series>
-              Episode <span class=position><?php $api->output($item->position); ?><span><?php if ($item->series_total): ?>/<span class=series-total><?php $api->output($item->series_total); ?></span><?php endif; ?><?php if ($item->series_position): ?>, Series <span class=series-position><?php $api->output($item->series_position); ?></span><?php endif; ?>
-              </div>  
-<?php endif; ?>
-          </td>
-      </tr>
-<?php endwhile; ?>     
-    </table>
-    
-    <nav>
-      <a id=more href="./?page=<?php print $page + 1; ?>">More</a>
-    </nav>
-  </div>
 </body>
 </html>
+
--- update.php
+++ update.php
@@ -3,37 +3,40 @@
 //if ($_ENV['_'] != '/usr/bin/php') exit(); // command line only
 
 require dirname(__FILE__) . '/main.php';
+Config::set('DEBUG', 'OFF');
 
 foreach (range(1,0) as $day){
   $date = date('Y/m/d', time() - (60*60*24*$day)); // yesterday's and today's listings
   //$date = date('Y/m/d');
 
-  $channels = array(
-    "http://www.bbc.co.uk/bbcone/programmes/schedules/london/$date.json",
-    "http://www.bbc.co.uk/bbctwo/programmes/schedules/england/$date.json",
-    "http://www.bbc.co.uk/bbcthree/programmes/schedules/$date.json",
-    "http://www.bbc.co.uk/bbcfour/programmes/schedules/$date.json",
+  $schedules = array(
+    'bbcone' => 'london',
+    'bbctwo' => 'england',
+    'bbcthree' => NULL,
+    'bbcfour' => NULL,
   );
 
-  foreach ($channels as $channel){
+  foreach ($schedules as $channel => $region){
     debug($channel);
-    $data = $api->get_data($channel, NULL, 'json');
-    
+    $api->cache = FALSE;
+    $data = $api->get_data("http://www.bbc.co.uk/$channel/programmes/schedules/$region/$date.json", NULL, 'json');
+    //debug($data);
+
     foreach ($data->schedule->day->broadcasts as $broadcast){
       if ($broadcast->is_repeat) continue;
-        
+
       $episode = $broadcast->programme;
       if (!$episode->media) continue;
-      
+
       $series = new stdClass;
       if ($episode->programme)
         $series = $episode->programme;
-          
+
       $db->query(
-      "INSERT IGNORE INTO episodes 
-      (`date`, episode, series, position, series_position, series_total, title, subtitle, synopsis) 
-      VALUES 
-      (%d, '%s', '%s', %d, %d, %d, '%s', '%s', '%s')", 
+      "INSERT IGNORE INTO episodes
+      (`date`, episode, series, position, series_position, series_total, title, subtitle, synopsis)
+      VALUES
+      (%d, '%s', '%s', %d, %d, %d, '%s', '%s', '%s')",
       strtotime($broadcast->start), $episode->pid, $series->pid, $episode->position, $series->position, $series->expected_child_count, $episode->display_titles->title, $episode->display_titles->subtitle, $episode->short_synopsis
       );
     }
@@ -41,3 +44,4 @@ foreach (range(1,0) as $day){
 }
 
 print "updated\n";
+
